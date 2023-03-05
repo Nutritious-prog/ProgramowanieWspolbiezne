0cadbccd4eba59cad625fe03903fc11c5c48f1ca
Wojciech Stobiński <tallandslim9@gmail.com>
2023-03-05 22:31:50 +0100

    Dodano projekt testowy

---
+++ Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    internal class Class1
+    {
+
+
+        public int Fib(int x)
+        {
+
+            if(x == 0)
+            {
+                return 0;
+            }
+
+            else if(x == 1)
+            {
+                return 1;
+            }
+
+            else
+            {
+                return Fib(x - 2) + Fib(x - 1);
+            }
+
+        }
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Test;
+
+Console.WriteLine("Hello, World!");
+
+
+Class1 test = new Class1();
+
+Console.WriteLine("Fibonnaci od 6 == " + test.Fib(6));
