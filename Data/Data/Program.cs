05b5dce0378fbfc3bcbdc28072d24081cf2212cd
Artur <artur.wlodarczyk12@gmail.com>
2023-03-30 14:39:28 +0200

    Stworzenie podziału na odpowiednie warstwy projektu.

--- Data/Data/Program.cs
+++ Data/Data/Program.cs
@@ -1,9 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using App;
-
 Console.WriteLine("Hello, World!");
-
-
-Class1 test = new Class1();
-
-Console.WriteLine("Fibonnaci od 6 == " + test.Fib(6));
